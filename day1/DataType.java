bbc323ade2b86397d1827a8acd96466971929187
Vinodshanmugam <112145073+Vinodshanmugam@users.noreply.github.com>
2025-06-22 09:48:06 +0530

    Add files via upload
    day1 changes

---
+++ day1/DataType.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		
+		float browserVersion = 100.2f;
+		String browserName = "firefox"; 
+		boolean isVisible = true;
+		int releaseYear = 1998;
+		char browserLogo = 'f'; 
+		
+		System.out.println(browserVersion+"\n "+browserName+"\n "+isVisible+" "+releaseYear+" "+browserLogo);
+	}
+}
